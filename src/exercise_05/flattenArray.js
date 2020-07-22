c1bab309d8431e21eb7b5e5361872310bab7928f
chentaiyu0336 <2868510447@qq.com>
2020-07-22 20:05:32 +0800

    [chentaiyu] feat: finish exercise5

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,21 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (!array) {
+    throw Error('Flatten undefined or null array');
+  }
+  if (!array.length) {
+    return array;
+  }
+  const flat = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i].length > 1) {
+      for (let j = 0; j < array[i].length; j += 1) {
+        flat.push(array[i][j]);
+      }
+    } else {
+      flat.push(array[i]);
+    }
+  }
+  return flat;
 }
