55b6f1ff8421730bbb90a6e0b46349cd2e66ee2c
chentaiyu0336 <2868510447@qq.com>
2020-07-22 18:42:23 +0800

    [chentaiyu] feat: finish exercise4.

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,23 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === null) {
+    throw Error('Creating list from undefined array');
+  }
+  if (array === undefined) {
+    throw Error('Creating list from undefined array');
+  }
+  if (!array.length) {
+    throw Error('Creating list from empty array');
+  }
+  let head = { value: array[0], next: null };
+  let list = head;
+  for (let i = 1; i < array.length; i += 1) {
+    list.next = { value: array[i], next: null };
+    if (i === 1) {
+      head = list;
+    }
+    list = list.next;
+  }
+  return head;
 }
